056ae652aa548262897210913b9a802ecf189bab
virdiignis <gbryk1@gmail.com>
2021-01-09 06:52:36 +0100

    try adding classes after 100ms

--- static/js/add_form_controls_classes.js
+++ static/js/add_form_controls_classes.js
@@ -1,8 +1,10 @@
-$("input:not([type=hidden])").addClass("form-control");
-$('select:not([class])').addClass("form-control");
-let inputfile = $("input:file");
-inputfile.addClass("form-control-file");
-inputfile.removeClass("form-control");
-let label = $("label")
-label.addClass("col-5");
-label.addClass("col-form-label");
+setTimeout(function () {
+    $("input:not([type=hidden])").addClass("form-control");
+    $('select:not([class])').addClass("form-control");
+    let inputfile = $("input:file");
+    inputfile.addClass("form-control-file");
+    inputfile.removeClass("form-control");
+    let label = $("label")
+    label.addClass("col-5");
+    label.addClass("col-form-label");
+}, 100);
