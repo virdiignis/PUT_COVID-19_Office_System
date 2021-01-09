e37119b64b8359b504af6640fe48b3e977be5c41
virdiignis <gbryk1@gmail.com>
2021-01-09 06:56:14 +0100

    temporaly disable adding form-control to selects

--- static/js/add_form_controls_classes.js
+++ static/js/add_form_controls_classes.js
@@ -1,10 +1,8 @@
-setTimeout(function () {
-    $("input:not([type=hidden])").addClass("form-control");
-    $('select:not([class])').addClass("form-control");
-    let inputfile = $("input:file");
-    inputfile.addClass("form-control-file");
-    inputfile.removeClass("form-control");
-    let label = $("label")
-    label.addClass("col-5");
-    label.addClass("col-form-label");
-}, 100);
+$("input:not([type=hidden])").addClass("form-control");
+// $('select:not([class])').addClass("form-control");
+let inputfile = $("input:file");
+inputfile.addClass("form-control-file");
+inputfile.removeClass("form-control");
+let label = $("label")
+label.addClass("col-5");
+label.addClass("col-form-label");
