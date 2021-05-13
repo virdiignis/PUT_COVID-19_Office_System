5817ae109059b1a4ff6f70ffb2a9508353dbcd3f
prance <gbryk1@gmail.com>
2021-05-13 07:05:05 +0200

    remove second shift takeover reminder

--- apps/office/tasks.py
+++ apps/office/tasks.py
@@ -10,14 +10,13 @@ from django.utils.translation import gettext_lazy as _
 def shift_takeover():
     now = localtime()
     takeover_deadline = now.replace(hour=now.hour + 1, minute=10, second=0)
-    for i in range(2):
-        Reminder.objects.create(
-            datetime=takeover_deadline,
-            set_by=Worker.objects.get(id=1),
-            title=_("Shift takeover"),
-            notes=_(
-                '''This is automatic reminder.
+    Reminder.objects.create(
+        datetime=takeover_deadline,
+        set_by=Worker.objects.get(id=1),
+        title=_("Shift takeover"),
+        notes=_(
+            '''This is automatic reminder.
 Accept it to indicate shift takeover.
 There is one of us for each of you, to record who was on the shift when.'''
-            )
         )
+    )
